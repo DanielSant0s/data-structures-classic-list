ceeb05facec3ea6e0b3efa75c841712ce82ec03a
Daniel Santos <47725160+DanielSant0s@users.noreply.github.com>
2023-03-17 03:34:02 -0300

    Isolate some functions

--- src/main.c
+++ src/main.c
@@ -6,9 +6,26 @@
 
 #define randint(min, max) ((rand() % (int)(((max) + 1) - (min))) + (min))
 
+void display_list(List* l) {
+    int value;
+
+    printf("[");
+    for (int i = 1; i <= list_size(l); i++) {
+        if (list_get(l, i, &value) == 0) {
+            printf("%d,", value);
+        }
+    }
+    printf("]\n");
+}
+
+void fill_with_random(List* l, int min, int max) {
+    for (int i = 1; i <= list_size(l); i++) {
+        list_insert(l, i, randint(min, max));
+    }
+}
+
 int main(int argc, char *argv[]) {
     List l;
-    int value;
     
     srand(time(NULL)); // for random int generation.
     setlocale(LC_ALL,"Portuguese");
@@ -20,21 +37,16 @@ int main(int argc, char *argv[]) {
         printf("A lista acabou de ser criada e está vazia.\n");
     }
 
-    // Fill list with random numbers
-    printf("Enchendo a lista com números aleatórios...\n");
-    for (int i = 1; i <= list_size(&l); i++) {
-        list_insert(&l, i, randint(0, 1000));
-    }
+    // Fill list with random numbers from 1 to 1000.
+    printf("Enchendo a lista com números aleatórios de 1 a 1000...\n");
+    fill_with_random(&l, 0, 1000);
 
-        if (list_full(&l)) {
+
+    if (list_full(&l)) {
         printf("A lista foi completamente preenchida.\n");
     }
 
-    for (int i = 1; i <= list_size(&l); i++) {
-        if (list_get(&l, i, &value) == 0) {
-            printf("pos %d - value %d\n", i, value);
-        }
-    }
+    display_list(&l);
 
 
     return 0;
